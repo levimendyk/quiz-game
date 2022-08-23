ba68ffd150f5707898188f3d3e0e65272d0c9e08
levimendyk <levi3743@gmail.com>
2022-08-23 08:24:53 -0600

    Added all questions to appear in order with choices, added a nav bar in highscores html to send back to home page

--- assets/js/app.js
+++ assets/js/app.js
@@ -82,29 +82,37 @@ function checkAnswer(e) {
 
 var questions = [
   {
-    question: "1Commonly used data types DO NOT include:",
+    question: "1.) Commonly used data types DO NOT include:",
     choices: ["Strings", "Booleans", "Alerts", "Numbers"],
     answer: "Alerts",
   },
   {
-    question: "2Commonly used data types DO NOT include:",
-    choices: ["Strings", "Booleans", "Alerts", "Numbers"],
-    answer: "Strings",
+    question:
+      "2.) The condition in an if / else statement is enclosed within ____.",
+    choices: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
+    answer: "Curly Brackets",
   },
   {
-    question: "3Commonly used data types DO NOT include:",
-    choices: ["Strings", "Booleans", "Alerts", "Numbers"],
-    answer: "Booleans",
+    question: "3.) Arrays in JavaScript can be used to store ____.",
+    choices: [
+      "Numbers and Strings",
+      "Other Arrays",
+      "Booleans",
+      "All of the Above",
+    ],
+    answer: "All of the Above",
   },
   {
-    question: "4Commonly used data types DO NOT include:",
-    choices: ["Strings", "Booleans", "Alerts", "Numbers"],
-    answer: "Numbers",
+    question:
+      "4.) String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["Commas", "Curly Brackets", "Quotes", "Parentheses"],
+    answer: "Quotes",
   },
   {
-    question: "5Commonly used data types DO NOT include:",
-    choices: ["Strings", "Booleans", "Alerts", "Numbers"],
-    answer: "Alerts",
+    question:
+      "5.) A very useful tool used during developement and debugging printing content to the debugger is ____.",
+    choices: ["JavaScript", "Terminal/Bash", "For Loops", "console.log"],
+    answer: "console.log",
   },
 ];
 
